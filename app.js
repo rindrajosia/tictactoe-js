1c710c438a7f165efc13dc9c9ab32c9c14661d11
rindra josia <rrindrajosia@gmail.com>
2020-09-17 18:54:08 +0300

    adding logic to check winner

--- app.js
+++ app.js
@@ -27,6 +27,34 @@ const checkGameStatus = () => {
      const bottomLeft = cellDivs[6].classList[2];
      const bottomMiddle = cellDivs[7].classList[2];
      const bottomRight = cellDivs[8].classList[2];
+
+     if(topLeft && topLeft === topMiddle && topLeft ===  topRight) {
+       handleWin(topLeft);
+     } else if (middleLeft && middleLeft === middleMiddle && middleLeft === middleRight) {
+         handleWin(middleLeft);
+     } else if (bottomLeft && bottomLeft === bottomMiddle && bottomLeft === bottomRight) {
+         handleWin(bottomLeft);
+     } else if (topLeft && topLeft === middleLeft && topLeft === bottomLeft) {
+         handleWin(topLeft);
+     } else if (topMiddle && topMiddle === middleMiddle && topMiddle === bottomMiddle) {
+         handleWin(topMiddle);
+     } else if (topRight && topRight === middleRight && topRight === bottomRight) {
+         handleWin(topRight)
+     } else if (topLeft && topLeft === middleMiddle && topLeft === bottomRight) {
+         handleWin(topLeft);
+     } else if (topRight && topRight === middleMiddle && topRight === bottomLeft) {
+         handleWin(topRight);
+     } else if (topLeft && topMiddle && topRight && middleLeft && middleMiddle && middleRight && bottomLeft && bottomMiddle && bottomRight) {
+         gameIsLive = false;
+         statusDiv.innerHTML = 'Game is tied!';
+     } else {
+         xIsNext = !xIsNext;
+         if (xIsNext) {
+             statusDiv.innerHTML = `${xSymbol} is next`;
+         } else {
+             statusDiv.innerHTML = `<span>${oSymbol} is next</span>`;
+         }
+     }
 }
 
 
