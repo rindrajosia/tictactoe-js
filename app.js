caccd125aca987f175fdd5cae406dd5632006121
edieatha <edieatha@gmail.com>
2020-09-17 18:45:37 +0300

    Added gameboard to array function

--- app.js
+++ app.js
@@ -16,3 +16,15 @@ const handleWin = (letter) => {
            statusDiv.innerHTML = `<span>${letterToSymbol(winner)} has won!</span>`;
          }
 };
+
+const checkGameStatus = () => {
+     const topLeft = cellDivs[0].classList[2];
+     const topMiddle = cellDivs[1].classList[2];
+     const topRight = cellDivs[2].classList[2];
+     const middleLeft = cellDivs[3].classList[2];
+     const middleMiddle = cellDivs[4].classList[2];
+     const middleRight = cellDivs[5].classList[2];
+     const bottomLeft = cellDivs[6].classList[2];
+     const bottomMiddle = cellDivs[7].classList[2];
+     const bottomRight = cellDivs[8].classList[2];
+}
