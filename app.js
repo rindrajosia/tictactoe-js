4000d7d10d2168dbf41451028681a5cd959a1937
edieatha <edieatha@gmail.com>
2020-09-19 02:39:38 +0300

    More refactoring

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ let xIsNext = true;
 let winner = null;
 
 const handleWin = (letter, arg1, arg2) => {
-  gameIsLive = false;
   winner = letter;
   if (winner === 'x') {
     statusDiv.innerHTML = `${arg1.getName()} has WON!`;
@@ -44,7 +43,6 @@ const checkGameStatus = (arg1, arg2) => {
   } else if (tpri && tpri === midmid && tpri === btmlft) {
     handleWin(tpri, arg1, arg2);
   } else if (tplft && tpmid && tpri && midlft && midmid && midri && btmlft && btmmid && btmri) {
-    gameIsLive = false;
     statusDiv.innerHTML = 'Game is tied!';
   } else {
     xIsNext = !xIsNext;
